c112324427d12022a35a9c11586c40e6d543b29d
Zeeshan <zeshan388@gmail.com>
2022-03-10 20:45:20 +0500

    commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send('this is node!!');
+    res.send('this is node!!!!!!');
 })
 
 app.listen(3000,()=> {
